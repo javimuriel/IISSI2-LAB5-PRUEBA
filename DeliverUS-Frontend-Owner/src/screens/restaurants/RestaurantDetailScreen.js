515fb8f382d2bcc929af85b03a935e6942669570
javimuriel <158613576+javimuriel@users.noreply.github.com>
2025-07-03 17:12:51 +0200

    Initial commit

---
+++ DeliverUS-Frontend-Owner/src/screens/restaurants/RestaurantDetailScreen.js
@@ -0,0 +1,105 @@
+/* eslint-disable react/prop-types */
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, View, FlatList, ImageBackground, Image, Pressable } from 'react-native'
+import { showMessage } from 'react-native-flash-message'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+import { getDetail } from '../../api/RestaurantEndpoints'
+import ImageCard from '../../components/ImageCard'
+import TextRegular from '../../components/TextRegular'
+import TextSemiBold from '../../components/TextSemibold'
+import * as GlobalStyles from '../../styles/GlobalStyles'
+import defaultProductImage from '../../../assets/product.jpeg'
+import { API_BASE_URL } from '@env'
+
+
+export default function RestaurantDetailScreen({ navigation, route }) {
+  const [restaurant, setRestaurant] = useState({})
+
+  useEffect(() => {
+    console.log('Loading restaurant, please wait 2 seconds')
+    setTimeout(() => {
+      const fetchedRestaurant = getDetail(route.params.id)
+      setRestaurant(fetchedRestaurant)
+      console.log('Restaurant loaded')
+    }, 2000)
+  }, [])
+
+  const renderHeader = () => {
+    return (
+      <ImageBackground source={(restaurant?.heroImage) ? { uri: API_BASE_URL + '/' + restaurant.heroImage, cache: 'force-cache' } : undefined} style={styles.imageBackground}>
+        <View style={styles.restaurantHeaderContainer}>
+          <TextSemiBold textStyle={styles.textTitle}>{restaurant.name}</TextSemiBold>
+          <Image style={styles.image} source={restaurant.logo ? { uri: API_BASE_URL + '/' + restaurant.logo, cache: 'force-cache' } : undefined} />
+          <TextRegular textStyle={styles.description}>{restaurant.description}</TextRegular>
+        </View>
+      </ImageBackground>
+    )
+  }
+
+  const renderProduct = ({ item }) => {
+    return (
+      <ImageCard
+        imageUri={item.image ? { uri: API_BASE_URL + '/' + item.image } : defaultProductImage}
+        title={item.name}
+      >
+        <TextRegular numberOfLines={2}>{item.description}</TextRegular>
+        <TextSemiBold textStyle={styles.price}>{item.price.toFixed(2)}€</TextSemiBold>
+        {!item.availability &&
+          <TextRegular textStyle={styles.availability}>Not available</TextRegular>
+        }
+      </ImageCard>
+    )
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        ListHeaderComponent={renderHeader}
+        style={styles.container}
+        data={restaurant.products}
+        renderItem={renderProduct}
+        keyExtractor={item => item.id.toString()}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  row: {
+    padding: 15,
+    marginBottom: 5,
+    backgroundColor: GlobalStyles.brandSecondary
+  },
+  restaurantHeaderContainer: {
+    height: 250,
+    padding: 20,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  imageBackground: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center'
+  },
+  image: {
+    height: 100,
+    width: 100,
+    margin: 10
+  },
+  description: {
+    color: 'white'
+  },
+  textTitle: {
+    fontSize: 20,
+    color: 'white'
+  },
+  availability: {
+    textAlign: 'right',
+    marginRight: 5,
+    color: GlobalStyles.brandSecondary
+  },
+})
---
+++ DeliverUS-Frontend-Owner/src/screens/restaurants/RestaurantsScreen.js
@@ -0,0 +1,86 @@
+/* eslint-disable react/prop-types */
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, View, FlatList, Pressable } from 'react-native'
+import TextRegular from '../../components/TextRegular'
+import { getAll } from '../../api/RestaurantEndpoints'
+import * as GlobalStyles from '../../styles/GlobalStyles'
+import ImageCard from '../../components/ImageCard'
+import TextSemiBold from '../../components/TextSemibold'
+import { API_BASE_URL } from '@env'
+import restaurantLogo from '../../../assets/restaurantLogo.jpeg'
+
+export default function RestaurantsScreen({ navigation }) {
+  const [restaurants, setRestaurants] = useState([])
+
+  useEffect(() => {
+    console.log('Loading restaurants, please wait 2 seconds')
+    setTimeout(() => {
+      setRestaurants(getAll) // getAll function has to be imported
+      console.log('Restaurants loaded')
+    }, 2000)
+  }, [])
+
+  const renderRestaurant = ({ item }) => {
+    return (
+      <Pressable
+        style={styles.row}
+        onPress={() => {
+          navigation.navigate('RestaurantDetailScreen', { id: item.id })
+        }}>
+        <TextRegular>
+          {item.name}
+        </TextRegular>
+      </Pressable>
+    )
+  }
+
+  const renderRestaurantWithImageCard = ({ item }) => {
+    return (
+      <ImageCard
+        imageUri={item.logo ? { uri: API_BASE_URL + '/' + item.logo } : restaurantLogo}
+        title={item.name}
+        onPress={() => {
+          navigation.navigate('RestaurantDetailScreen', { id: item.id })
+        }}
+      >
+        <TextRegular numberOfLines={2}>{item.description}</TextRegular>
+        {item.averageServiceMinutes !== null &&
+          <TextSemiBold>Avg. service time: <TextSemiBold textStyle={{ color: GlobalStyles.brandPrimary }}>{item.averageServiceMinutes} min.</TextSemiBold></TextSemiBold>
+        }
+        <TextSemiBold>Shipping: <TextSemiBold textStyle={{ color: GlobalStyles.brandPrimary }}>{item.shippingCosts.toFixed(2)}€</TextSemiBold></TextSemiBold>
+      </ImageCard>
+    )
+  }
+
+
+  return (
+    <FlatList
+      style={styles.container}
+      data={restaurants}
+      renderItem={renderRestaurantWithImageCard}
+      keyExtractor={item => item.id.toString()}
+    />
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  actionButton: {
+    borderRadius: 8,
+    height: 40,
+    marginTop: 12,
+    margin: '1%',
+    padding: 10,
+    alignSelf: 'center',
+    flexDirection: 'column',
+    width: '50%'
+  },
+  text: {
+    fontSize: 16,
+    color: 'white',
+    alignSelf: 'center',
+    marginLeft: 5
+  }
+})
